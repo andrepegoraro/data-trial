41c8851a057bba059c308dadc36c9fe5f28c81e5
dev_andrepn <andre.pegoraro@indicium.tech>
2024-10-20 23:17:28 -0300

    all raw data cleaned. Now basic transformations are performed within pgadmin UI and later gonna be tested in notebooks

--- dags/clever_main_DAG.py
+++ dags/clever_main_DAG.py
@@ -17,10 +17,12 @@ datasets = [
     # 'fmcsa_company_snapshot.csv',
     # 'fmcsa_companies.csv',
     # 'customer_reviews_google_filtered.csv',
-    'company_profiles_google_maps_filtered.csv'
+    # 'company_profiles_google_maps_filtered.csv'
+    'customer_reviews_escaped_filtered.csv'
+    # 'company_profiles_escaped.csv'
 ]
 
-with DAG("clever_main_DAG", default_args=default_args, catchup=False, schedule_interval='30 22 * * *', max_active_runs=1) as dag:
+with DAG("clever_main_DAG", default_args=default_args, catchup=False, schedule_interval='00 02 * * *', max_active_runs=1) as dag:
 
     start_task = EmptyOperator(task_id='Start', dag=dag)
     finish_task = EmptyOperator(task_id='Finish', dag=dag)
--- dags/scripts/customer_reviews_clean.py
+++ dags/scripts/customer_reviews_clean.py
@@ -2,7 +2,12 @@ import pandas as pd
 
 def cleaning_customer_reviews(file_path, output_path):
     df = pd.read_csv(file_path, delimiter=',', quotechar='"', on_bad_lines='skip')
-    columns_to_drop = ['review_text', 'review_photo_ids', 'owner_answer', 'review_link']
+    columns_to_drop = [
+        'review_text'
+        , 'review_photo_ids'
+        # , 'owner_answer'
+        # , 'review_link'
+    ]
     df_cleaned = df.drop(columns=[col for col in columns_to_drop if col in df.columns], axis=1)
 
     # Keep only rows where 'google_id' starts with '0x'
@@ -16,3 +21,7 @@ def cleaning_customer_reviews(file_path, output_path):
 input_google = 'dags/scripts/data_examples/customer_reviews_google.csv'
 output_google = 'dags/scripts/data_examples/customer_reviews_google_filtered.csv'
 customer_reviews_df = cleaning_customer_reviews(input_google, output_google)
+
+input_google2 = 'dags/scripts/data_examples/customer_reviews_escaped.csv'
+output_google2 = 'dags/scripts/data_examples/customer_reviews_escaped_filtered.csv'
+customer_reviews_df2 = cleaning_customer_reviews(input_google2, output_google2)
